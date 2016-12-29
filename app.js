9bffb686d383cfbb8ac4ff6156fbbf536ec87a10
Your Name <you@example.com>
2016-12-29 23:22:42 +0100

    app.js mdified

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ var express = require('express');
 var mongoose=require('mongoose');
 var bodyParser = require('body-parser');
 var Contact= require('./contactController')
-mongoose.connect('mongodb://example:example@ds053312.mongolab.com:53312/todolist');
+mongoose.connect('mongodb://localhost/test');
 var app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(__dirname+ '/public')
 app.get('/api', Contact.lists)
 app.post('/api',Contact.add)
 app.route('/api/:id')
 .get(Contact.list)
 .put(Contact.update)
 .delete(Contact.delete)
-app.listen(process.env.PORT ||3000, function () {
+
+
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
